380bc23cccf3b43826f592365e11a1e9bdebcfb8
Vasiliy Volgin <82467534+VasiliiV@users.noreply.github.com>
2022-07-14 11:22:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,41 @@ const buttom = document.getElementsByTagName('button')[0];
 
 input.addEventListener('input', (event) => {
     const pass = event.target.value;
-   
-    if (pass.length < 8) {
-        text.innerText = 'max 8 symbols';
-        text.classList.remove('red--password')
+
+//отрабатывает либо буквенная, либо проверка чисел...
+    if (pass.length < 8){
+        text.innerText = 'min 8 symbols';
+        text.classList.remove('green--password')
         buttom.disabled = true
-    } else {
-        text.innerText = 'password strong';
+    } else if (!CheckingForBigLetters(pass)){
+        text.innerText = 'use uppercase letters';
+        buttom.disabled = true
         text.classList.add('red--password')
+    } else if (!isNaN(pass) === false){
+        text.innerText = 'use number';
+        buttom.disabled = true;
+        text.classList.add('red--password')
+    }
+    else {
+        text.innerText = 'password strong';
+        text.classList.add('green--password')
         buttom.disabled = false
-    } 
+    }
 })
+
+//функция циклится и проверяет каждую букву с условием - есть ли большая буква и возвращает false
+function CheckingForBigLetters(string) {
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i];
+        if (char === char.toUpperCase()) {
+            return true
+        };
+    }
+    return false
+}
+
+//Еще один способ проверка чисел в строке
+
+/* function NunNumber(myString) {
+    return /\d/.test(myString);
+} */
